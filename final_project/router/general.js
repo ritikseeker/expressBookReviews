f9c625ec592e913a976674bb68287aba32c00623
ritikseeker <ritikraturiworks@gmail.com>
2025-05-31 09:21:17 -0400

    Fetching books based on ISBN no. functionality added

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,10 +17,18 @@ public_users.get('/',function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    // Retrieve the ISBN from the request parameters
+    const isbn = req.params.isbn;
+  
+    // Check if the book with the given ISBN exists
+    if (books[isbn]) {
+      return res.status(200).json(books[isbn]);
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  });
+  
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
